f620efcbed5be7d01342e6e619f7b957506a6d1e
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-01-23 12:23:47 -0800

    soto

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -20,18 +20,18 @@ void selection_sort(int *array, size_t size)
 	{
 		curr = (array + i);
 		min = (array + i);
-	for (j = i + 1; j < size; j++)
-	{
-		if (*(array + j) < *min)
-			min = (array + j);
-	}
+		for (j = i + 1; j < size; j++)
+		{
+			if (*(array + j) < *min)
+				min = (array + j);
+		}
 
-	if (min != curr)
-	{
-		temp = *curr;
-		*curr = *min;
-		*min = temp;
-		print_array(array, size);
-	}
+		if (min != curr)
+		{
+			temp = *curr;
+			*curr = *min;
+			*min = temp;
+			print_array(array, size);
+		}
 	}
 }	
