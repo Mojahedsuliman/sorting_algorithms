34616bec3f06260822d0b26f622d41cfb0e54ee6
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-01-24 10:27:40 -0800

    pop

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,1 +1,17 @@
 #include "sort.h"
+
+/*
+ * insertion_sort_list - a function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
+ * @list: the pointer to the head of the doubly linked list
+ *
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *curr;
+	int key, value;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
